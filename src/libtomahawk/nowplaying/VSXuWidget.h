6f10796d5dd722eecfe074d11ff3ff78dbaeffeb
Dinesh Manajipet <saidinesh5@gmail.com>
2012-02-12 05:30:55 +0530

    Added Code to the VSXuWidget to recieve audiodataoutput from the audioengine and enabled audiodataoutput in the audioengine.

--- src/libtomahawk/nowplaying/VSXuWidget.h
+++ src/libtomahawk/nowplaying/VSXuWidget.h
@@ -1,10 +1,13 @@
 #ifndef VSXuWidget_H
 #define VSXuWidget_H
 
-#include <vsx_manager.h>
 #include <QGLWidget>
 #include <QTimer>
 #include <QStringList>
+
+#include <phonon/audiodataoutput.h>
+#include <phonon/path.h>
+#include <vsx_manager.h>
 
 class VSXuWidget : public QGLWidget
 {
@@ -12,10 +15,13 @@ class VSXuWidget : public QGLWidget
 private:
   vsx_manager_abs* m_manager;
   QTimer *m_timer; // A Timer to frequently update the display
-  
+
   QStringList m_visuals;
   int m_width,m_height;
   bool m_isActive; // To Save the CPU when we aren't using VSXu.
+  QMap< Phonon::AudioDataOutput::Channel, QVector< qint16 > >     m_audioData;
+
+  void injectSound(float soundData[]);
 
 protected:
   void initializeGL();
@@ -28,7 +34,9 @@ protected:
 public:
   VSXuWidget(QWidget *parent = NULL);
   ~VSXuWidget();
-  void injectSound(float soundData[]);
+  
+public slots:
+  void receiveData( const QMap<Phonon::AudioDataOutput::Channel,QVector<qint16> >& data );
 };
 
 #endif // VSXuWidget_H
